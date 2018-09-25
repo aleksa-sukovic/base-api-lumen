5e35ab06e2deb961fece6ee1fcd3bdcb26ef071b
Aleksa Sukovic <aleksa.sukovic@fleka.me>
2018-09-25 10:18:20 +0200

    Adds lumen framework to application
    This is mostly clear lumen install with added base, object controllers and Fractal library.

---
+++ app/Library/Controllers/BaseController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Library\Controllers;
+
+use App\Http\Controllers\Controller;
+use League\Fractal\Manager;
+use League\Fractal\Serializer\ArraySerializer;
+use League\Fractal\Resource\Item;
+use League\Fractal\TransformerAbstract;
+
+class BaseController extends Controller
+{
+    protected $statusCode;
+    protected $transformer;
+    protected $fractal;
+    protected $model;
+
+    public function __construct()
+    {
+        $this->fractal = new Manager();
+        $this->fractal->setSerializer(new ArraySerializer);
+    }
+
+    public function respondSingle($data, string $message = 'Success', array $additionalData = [], $customTransformer = null)
+    {
+        $transformer = $this->transformer;
+        if ($customTransformer) {
+            $transformer = $customTransformer;
+        }
+
+        $item = new Item($data, $transformer);
+        return $this->respond($this->fractal->createData($item)->toArray(), $message, $additionalData);
+    }
+
+    private function respond($data, string $message = 'Success', array $additionalData = [])
+    {
+        $responseArray = [
+            'message' => $message,
+            'status_code' => $this->getStatusCode(),
+            'data' => $data
+        ];
+
+        foreach ($additionalData as $key => $value) {
+            $responseArray[$key] = $value;
+        }
+
+        return response()->json($responseArray)->setStatusCode($this->getStatusCode());
+    }
+
+    public function getStatusCode()
+    {
+        return $this->statusCode;
+    }
+
+    public function setStatusCode(int $statusCode)
+    {
+        $this->statusCode = $statusCode;
+    }
+}
---
+++ app/Library/Controllers/ObjectController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Library\Controllers;
+
+class ObjectController extends BaseController
+{
+    public function index()
+    {
+    }
+
+    public function show($id)
+    {
+    }
+
+    public function store()
+    {
+    }
+
+    public function update($id)
+    {
+    }
+
+    public function delete($id)
+    {
+    }
+}
