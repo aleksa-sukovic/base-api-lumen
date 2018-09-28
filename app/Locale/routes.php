83668d095c310e7f1d1b3e4ff6e93929a934f239
Aleksa Sukovic <aleksa.sukovic@fleka.me>
2018-09-28 13:26:01 +0200

    Adds routes to Locale object

--- app/Locale/routes.php
+++ app/Locale/routes.php
@@ -3,3 +3,7 @@
 $app = app();
 
 $app->router->get('/locales', 'Aleksa\Locale\Controllers\LocaleController@index');
+$app->router->get('/locales/{id:[0-9]+}', 'Aleksa\Locale\Controllers\LocaleController@show');
+$app->router->post('/locales', 'Aleksa\Locale\Controllers\LocaleController@store');
+$app->router->put('/locales/{id:[0-9]+}', 'Aleksa\Locale\Controllers\LocaleController@update');
+$app->router->delete('/locales/{id:[0-9]+}', 'Aleksa\Locale\Controllers\LocaleController@destroy');
