e3934a212684381bfad9c74dfc080cb7798352ab
Aleksa Sukovic <aleksa.sukovic@fleka.me>
2018-10-01 14:25:10 +0200

    Adds LocaleRepository

--- app/Locale/Repositories/LocaleRepository.php
+++ app/Locale/Repositories/LocaleRepository.php
@@ -2,15 +2,17 @@
 
 namespace Aleksa\Locale\Repositories;
 
-use Aleksa\Library\Repositories\ObjectRepository;
 use Aleksa\Locale\Models\Locale;
 use Aleksa\Locale\Validators\LocaleValidator;
+use Aleksa\Library\Repositories\ObjectRepository;
+use Aleksa\Locale\Processors\LocaleQueryProcessor;
 
 class LocaleRepository extends ObjectRepository
 {
-    public function __construct(Locale $model, LocaleValidator $validator)
+    public function __construct(Locale $model, LocaleValidator $validator, LocaleQueryProcessor $queryProcessor)
     {
-        $this->model     = $model;
-        $this->validator = $validator;
+        $this->model          = $model;
+        $this->validator      = $validator;
+        $this->queryProcessor = $queryProcessor;
     }
 }
