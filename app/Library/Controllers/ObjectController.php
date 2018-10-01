d6340a2a141a48974133dbf6faed5736e991733f
Aleksa Sukovic <aleksa.sukovic@fleka.me>
2018-10-01 10:58:28 +0200

    Implements show, store, update and destroy method inside ObjectController

--- app/Library/Controllers/ObjectController.php
+++ app/Library/Controllers/ObjectController.php
@@ -13,17 +13,33 @@ class ObjectController extends BaseController
 
     public function show($id)
     {
+        $item = $this->repository->findById($id, true);
+        return $this->respondSingle($item);
     }
 
     public function store()
     {
+        $params = $this->request->all();
+
+        $item = $this->repository->create($params);
+
+        $item->save();
+        return $this->respondSingle($item, 201);
     }
 
     public function update($id)
     {
+        $params = $this->request->all();
+
+        $item = $this->repository->update($id, $params);
+
+        return $this->respondSingle($item, 200);
     }
 
     public function destroy($id)
     {
+        $item = $this->repository->delete($id);
+
+        return $this->respondSingle($item, 200);
     }
 }
