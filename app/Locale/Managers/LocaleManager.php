901892f3ba92e1ad9d21f4b075d4f04f2aaddb53
Aleksa Sukovic <aleksa.sukovic@fleka.me>
2018-11-16 14:13:35 +0100

    Adds LocaleManager class
    Class is responsible for managing locales. It allows setting new locales as long as they are present in database. If locale is not present in database, default locale is used.

---
+++ app/Locale/Managers/LocaleManager.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Aleksa\Locale\Managers;
+
+class LocaleManager
+{
+    private static $defaultLocaleCode = 'en';
+
+    private static $locale     = null;
+    private static $repository = null;
+
+    public static function code()
+    {
+        return self::locale()->code;
+    }
+
+    public static function locale($locale = null)
+    {
+        if ($locale !== null) {
+            return self::setLocale($locale);
+        }
+
+        return self::$locale !== null ? self::$locale : self::getDefaultLocale();
+    }
+
+    private static function setLocale($localeCode)
+    {
+        $repository = self::getRepository();
+        $locale     = $repository->findByCode($localeCode);
+
+        if ($locale) {
+            return (self::$locale = $locale);
+        }
+
+        return self::$locale !== null ? self::$locale : self::getDefaultLocale();
+    }
+
+    private static function getDefaultLocale()
+    {
+        $repository = self::getRepository();
+
+        self::$locale = $repository->findByCode(self::$defaultLocaleCode);
+
+        return self::$locale;
+    }
+
+    private static function getRepository()
+    {
+        if (self::$repository === null) {
+            self::$repository = app()->make('Aleksa\Locale\Repositories\LocaleRepository');
+        }
+
+        return self::$repository;
+    }
+}
