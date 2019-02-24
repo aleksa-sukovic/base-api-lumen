765756255a179339603c7633da5f592e2acc8bfd
Aleksa Sukovic <sukovic.aleksa@gmail.com>
2019-02-24 14:32:50 +0100

    Adds reauth_requested_at field to User object

---
+++ app/User/Database/Migrations/2019_02_24_133008_add_reauth_requested_at_field_to_users_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddReauthRequestedAtFieldToUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dateTime('reauth_requested_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('reauth_requested_at');
+        });
+    }
+}
