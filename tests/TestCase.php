3fef7f74f74000039dadc6ed8df2360a728e28a7
Aleksa Sukovic <sukovic.aleksa@gmail.com>
2019-03-09 21:54:16 +0100

    Adds 'actAs...' helper methods to TestCase base class

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,6 +4,8 @@ namespace Tests;
 
 use Laravel\Lumen\Testing\TestCase as Test;
 use Laravel\Lumen\Testing\DatabaseMigrations;
+use Aleksa\Library\Facades\Auth;
+use Aleksa\User\Models\User;
 
 class TestCase extends Test
 {
@@ -20,5 +22,33 @@ class TestCase extends Test
     public function test_working()
     {
         $this->assertTrue(true);
+    }
+
+    public function actAsSuperAdmin()
+    {
+        $user = factory(User::class)->state('super-admin')->create();
+
+        Auth::setUser($user);
+    }
+
+    public function actAsAdmin()
+    {
+        $user = factory(User::class)->state('admin')->create();
+
+        Auth::setUser($user);
+    }
+
+    public function actAsEditor()
+    {
+        $user = factory(User::class)->state('editor')->create();
+
+        Auth::setUser($user);
+    }
+
+    public function actAsUser()
+    {
+        $user = factory(User::class)->state('user')->create();
+
+        Auth::setUser($user);
     }
 }
