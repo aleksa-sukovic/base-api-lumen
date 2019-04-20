31a9ff950e65f9cdd0928a90c1255eb9d022036a
Aleksa Sukovic <sukovic.aleksa@gmail.com>
2019-04-20 16:58:18 +0200

    Adds UserGroup policy class

--- app/UserGroup/UserGroupServiceProvider.php
+++ app/UserGroup/UserGroupServiceProvider.php
@@ -4,6 +4,9 @@ namespace Aleksa\UserGroup;
 
 use Illuminate\Support\ServiceProvider;
 use Aleksa\UserGroup\Database\Factories\UserGroupModelFactory;
+use Aleksa\UserGroup\Policies\UserGroupPolicy;
+use Illuminate\Support\Facades\Gate;
+use Aleksa\UserGroup\Models\UserGroup;
 
 class UserGroupServiceProvider extends ServiceProvider
 {
@@ -19,6 +22,8 @@ class UserGroupServiceProvider extends ServiceProvider
         if (in_array(app()->environment(), ['local', 'staging', 'testing'])) {
             app(UserGroupModelFactory::class)->register(null);
         }
+
+        Gate::policy(UserGroup::class, UserGroupPolicy::class);
 
         include __DIR__ . '/routes.php';
     }
