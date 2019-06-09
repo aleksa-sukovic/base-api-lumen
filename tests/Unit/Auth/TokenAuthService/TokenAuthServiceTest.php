d7550ff7e50fa7c890808a8e81d097ca01dd5d93
Aleksa Šuković <sukovic.aleksa@gmail.com>
2019-06-09 21:18:04 +0200

    Removes old tests from TokenAuthServiceTest file

--- tests/Unit/Auth/TokenAuthService/TokenAuthServiceTest.php
+++ tests/Unit/Auth/TokenAuthService/TokenAuthServiceTest.php
@@ -50,75 +50,6 @@ class TokenAuthServiceTest extends TestCase
         Mockery::close();
     }
 
-    public function test_request_authentication_succeeded()
-    {
-        $this->request->shouldReceive('header')
-            ->once()
-            ->with('al-access-token')
-            ->andReturn($this->tokenManager->generate($this->user)['token']);
-
-        $this->authService->authenticateRequest($this->request);
-
-        $this->assertEquals($this->user->id, Auth::getUser()->id);
-    }
-
-    public function test_request_authentication_missing_token()
-    {
-        $this->request->shouldReceive('header')
-            ->once()
-            ->with('al-access-token')
-            ->andReturnNull();
-
-        $this->expectException('Aleksa\Library\Exceptions\AuthException');
-
-        $this->authService->authenticateRequest($this->request);
-    }
-
-    public function test_request_authentication_invalid_token()
-    {
-        $this->request->shouldReceive('header')
-            ->once()
-            ->with('al-access-token')
-            ->andReturn('this_is_invalid_token_data');
-
-        $this->expectException('Aleksa\Library\Exceptions\TokenException');
-
-        $this->authService->authenticateRequest($this->request);
-    }
-
-    public function test_request_authentication_user_not_exists()
-    {
-        $user = factory(User::class)->make(['id' => 10]);
-        $token = $this->tokenManager->generate($user)['token'];
-        $this->request->shouldReceive('header')
-            ->once()
-            ->with('al-access-token')
-            ->andReturn($token);
-
-        $this->expectException('Aleksa\Library\Exceptions\ItemNotFoundException');
-
-        $this->authService->authenticateRequest($this->request);
-    }
-
-    public function test_request_token_revoked()
-    {
-        $this->request->shouldReceive('header')
-            ->once()
-            ->with('al-access-token')
-            ->andReturn($this->tokenManager->generateToken(
-                $this->user,
-                Carbon::now()->subHours(2)->timestamp,
-                Carbon::now()->addHours(4)->timestamp
-            )['token']);
-
-        $this->user->reauth_requested_at = Carbon::now();
-        $this->user->save();
-
-        $this->expectException('Aleksa\Library\Exceptions\TokenException');
-
-        $this->authService->authenticateRequest($this->request);
-    }
-
     public function test_user_authentication_success()
     {
         $this->request->shouldReceive('all')->once()->andReturn([
@@ -163,22 +94,6 @@ class TokenAuthServiceTest extends TestCase
         $this->authService->authenticateUser($this->request);
     }
 
-    public function test_token_refresh_success()
-    {
-        $this->request->shouldReceive('header')
-            ->once()
-            ->with('al-access-token')
-            ->andReturn($this->tokenManager->generate($this->user)['token']);
-
-        $data = $this->authService->refreshAuthentication($this->request);
-
-        $this->assertNotNull($data);
-        $this->assertArrayHasKey('token', $data);
-
-        $token = $this->tokenManager->decode($data['token']);
-        $this->assertEquals($this->user->id, $token['id']);
-    }
-
     public function test_token_refresh_failure()
     {
         $this->request->shouldReceive('header')->once()
@@ -187,65 +102,5 @@ class TokenAuthServiceTest extends TestCase
         $this->expectException('Aleksa\Library\Exceptions\AuthException');
 
         $this->authService->refreshAuthentication($this->request);
-    }
-
-    public function test_revoke_authentication_success()
-    {
-        $token = $this->tokenManager->generate($this->user)['token'];
-        $this->request->shouldReceive('header')->once()
-            ->with('al-access-token')->andReturn($token);
-
-        $this->authService->revokeAuthentication($this->request);
-        $this->user->refresh();
-
-        $this->assertNotNull($this->user->reauth_requested_at);
-        $this->assertEquals(0, Carbon::now()->diffInHours(
-            Carbon::createFromTimeString($this->user->reauth_requested_at)
-        ));
-    }
-
-    public function test_reset_credentials_succeeded()
-    {
-        $this->request->shouldReceive('header')->with('al-access-token')->once()
-            ->andReturn($this->tokenManager->generate($this->user)['token']);
-        $this->request->shouldReceive('all')->once()->andReturn([
-            'password'              => 'changed',
-            'password_confirmation' => 'changed',
-            'old_password'          => 123123
-        ]);
-
-        $this->authService->resetCredentials($this->request);
-        $this->user->refresh();
-
-        $this->assertTrue(Hash::check('changed', $this->user->password));
-    }
-
-    public function test_reset_credentials_password_mismatch()
-    {
-        $this->request->shouldReceive('header')->once()->andReturn($this->tokenManager->generate($this->user)['token']);
-        $this->request->shouldReceive('all')->once()->andReturn([
-            'password'              => 'changed',
-            'password_confirmation' => 'changed_wrong',
-            'old_password'          => 123123
-        ]);
-
-        $this->expectException('Aleksa\Library\Exceptions\ValidationException');
-
-        $this->authService->resetCredentials($this->request);
-    }
-
-    public function test_reset_credentials_old_password_mismatch()
-    {
-        $this->request->shouldReceive('header')->with('al-access-token')->once()
-            ->andReturn($this->tokenManager->generate($this->user)['token']);
-        $this->request->shouldReceive('all')->once()->andReturn([
-            'password'              => 'changed',
-            'password_confirmation' => 'changed',
-            'old_password'          => 'wrong_old_password'
-        ]);
-
-        $this->expectException('Aleksa\Library\Exceptions\AuthException');
-
-        $this->authService->resetCredentials($this->request);
     }
 }
